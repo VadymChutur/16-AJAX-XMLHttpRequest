d50cbf99865e1f6ca8525ef18c253c7281c628ac
Vadym Chutur <falloutgreck@gmail.com>
2024-08-22 14:59:12 +0300

    Update ajax.js

--- app/src/js/ajax.js
+++ app/src/js/ajax.js
@@ -1,1 +1,49 @@
+'use strict';
+
 console.log('test');
+// function foo() {
+//   console.log(this);
+// }
+
+// foo();
+
+// const boo = function () {
+//   console.log(this);
+// };
+
+// boo();
+
+// const arrow = () => this;
+
+// console.log(arrow());
+
+const objA = {
+  name: 'User name',
+  skills: {
+    mySkill: 'html, css, sass, js, webpack',
+    someValue: {
+      value: 10,
+      nickName() {
+        console.log(this);
+      },
+    },
+  },
+};
+
+// objA.nickName();
+objA.skills.someValue.nickName();
+
+const objB = {
+  name: 'Nick name',
+  skills: {
+    mySkill: 'html, css, sass, js, webpack',
+    foo: () => {
+      function nickName() {
+        console.log(this);
+      }
+      nickName();
+    },
+  },
+};
+
+objB.skills.foo();
